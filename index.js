71ab16b677714cc6c627af2d3a74056b827102ee
Augusto Seta <alseta97@gmail.com>
2023-02-10 15:47:15 -0300

    Added an error handler. The functionality of deleting or modifying a telephone number from the database will be enabled.

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ morgan.token('bodyJSON', (request, resolve) => {
   return JSON.stringify(request.body)
 })
 
+
+app.use(express.static('build'));
+app.use(express.json())
 // custom console messages
 app.use(morgan((tokens, request, resolve) => {
   return tokens.method(request, resolve) !== 'POST'
@@ -32,54 +35,18 @@ app.use(morgan((tokens, request, resolve) => {
      ].join(' ')
 }))
 
-app.use(express.json())
-app.use(express.static('build'));
-
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122'
-  }
-]
-
-const getPersonsAmount = () => {
-  return persons.length
-}
-
 const getDate = () => {
   return new Date()
 }
 
-const generateRandomId = () => {
-  const randomId = Math.round(Math.random() * 1000)
-  return randomId
-}
-
-app.get('/', (request, response) => {
-  response.send('<h1>Welcome to my Phonebook API REST</h1>')
-})
-
 app.get('/info', (request, response) => {
-  response.send(`
-    <p><b>Phonebook has info for ${getPersonsAmount()} persons.</b></p>
-    <p><b>${getDate()}</b></p>
-    `);
+  Person.find({}).then(persons => {
+    response.send(
+      `<p><b>Phonebook has info for ${persons.length} persons<b/></b>
+       <p><b>${getDate()}</b></p>
+      `
+    )
+  })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -88,17 +55,24 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id );
-  
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -122,6 +96,39 @@ app.post('/api/persons', (request, response) => {
   })
 
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response, next) => {
+  return response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  console.log(error.name);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
